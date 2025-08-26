f35802235b61fe487173059b953ebc784dde683a
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2025-08-26 18:42:03 +0530

    fix flash image issues

--- src/Component/FlashDeal.jsx
+++ src/Component/FlashDeal.jsx
@@ -25,37 +25,37 @@ function FlashDeal() {
   return (
     <>
 
-      <div className="flex justify-center my-3 md:my-5 relative w-full h-lvh bg-cover bg-center bg-[url('/img/FlashDeal.png')]">
+      <div className="flex justify-center my-3 md:my-5 relative w-full min-h-screen bg-cover bg-center bg-[url('/img/FlashDeal.png')]">
 
         <div className="my-10 absolute">
           <h1 className="font-bold text-6xl text-white">FLASH DEAL</h1>
           <p className="my-5 mx-14 bg-white px-12 py-1 text-gray-800 rounded-xs">🕓 Ends in 01 day</p>
         </div>
 
-        <Slider {...settings} className="overflow-hidden absolute mx-10">
+        <Slider {...settings} className="overflow-hidden mx-10 h-full w-full absolute">
           {Array.isArray(data.Data) && data.Data.map((img) => (
             <div className="mt-50">
 
               <img
                 src={img.imageUrl}
-                className="w-[430px] mx-6 mt-6 object-cover"
+                className="w-[410px] mx-6 mt-6 object-cover"
                 alt={error}
               />
 
               <img
                 src={img.brandLogo}
-                className="absolute bottom-[40%] w-[130px] h-[50px] mx-10 object-cover"
+                className="absolute bottom-[38%] w-[130px] h-[50px] mx-10 object-cover"
 
               />
 
-              <button className="mx-20 cursor-pointer inline rounded-lg px-3 py-1 bg-blue-700 text-white font-bold text-lg">Grab deal</button>
+              <button className="mx-78 absolute bottom-6 cursor-pointer rounded-lg px-3 py-1 bg-blue-700 text-white font-bold text-lg">Grab deal</button>
 
             </div>
 
           ))}
 
         </Slider>
-        <button className="absolute bottom-10 left-[40%] bg-gray-800 rounded-xl opacity-70 px-30 py-2 text-white font-extrabold text-xl">View All</button>
+        <button className="absolute cursor-pointer bottom-10 left-[40%] bg-gray-800 rounded-xl opacity-70 px-30 py-2 text-white font-extrabold text-xl">View All</button>
 
       </div>
 
